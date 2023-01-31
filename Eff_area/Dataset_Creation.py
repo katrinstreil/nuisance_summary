ece7dfbc79fa76ddd69164e043491bfef5177401
katrinstreil <katrin.streil@fau.de>
2023-01-31 16:23:33 +0100

    make changing models the primary

--- Eff_area/Dataset_Creation.py
+++ Eff_area/Dataset_Creation.py
@@ -3,9 +3,13 @@ from gammapy.modeling.models import (
     Models, 
     FoVBackgroundModel,
     PowerLawSpectralModel,
-    SkyModel)
-from MapDatasetNuisanceE import MapDatasetNuisanceE
+    SkyModel,
+    PowerLawNuisanceSpectralModel,
+    PowerLawNormNuisanceSpectralModel)
+#from MapDatasetNuisanceE import MapDatasetNuisanceE
 from gammapy.modeling import Parameter, Parameters
+from gammapy.datasets import MapDataset
+
 path_crab = '/home/hpc/caph/mppi045h/3D_analysis/N_parameters_in_L/nuisance_summary/Crab'
 
 
@@ -28,6 +32,8 @@ class sys_dataset():
         source_model = SkyModel(spatial_model = models['main source'].spatial_model ,
                                spectral_model = model_spectrum,
                                name = "Source")    
+        source_model.parameters['lon_0'].frozen = True
+        source_model.parameters['lat_0'].frozen = True
         models = Models(source_model)
         return models
     
@@ -36,7 +42,7 @@ class sys_dataset():
         exposure = dataset.exposure.copy()
         exposure.data *= (1-self.factor)
         background = dataset.background.copy()
-        background.data *= (1-self.factor)
+        #background.data *= (1-self.factor)
         dataset.exposure = exposure
         dataset.background = background
         if self.rnd:
@@ -53,11 +59,28 @@ class sys_dataset():
         return dataset
     
     
-    def create_dataset_N(self, sigma):
+    def set_model_N(self):
+        models = Models.read(f"{path_crab}/standard_model.yml").copy()
+        model_spectrum  = PowerLawNuisanceSpectralModel(
+            index=2.3,
+            amplitude="1e-12 TeV-1 cm-2 s-1",  
+            amplitudeN = 0)
+        print(len(model_spectrum.parameters))
+        print(len(model_spectrum.default_parameters))
+
+        source_model = SkyModel(spatial_model = models['main source'].spatial_model ,
+                               spectral_model = model_spectrum,
+                               name = "SourceN")  
+        source_model.parameters['lon_0'].frozen = True
+        source_model.parameters['lat_0'].frozen = True
+        models = Models(source_model)
+        return models
+
+
+
+    def create_dataset_N(self):
         dataset_ = self.create_dataset()
-        N_parameter = Parameter(name = "effarea", value = 0)
-        N_parameters = Parameters([N_parameter])
-        dataset_N = MapDatasetNuisanceE (
+        dataset_N = MapDataset(
                 counts=dataset_.counts.copy(),
                 exposure=dataset_.exposure.copy(),
                 background=dataset_.background.copy(),
@@ -65,11 +88,15 @@ class sys_dataset():
                 edisp=dataset_.edisp.copy(),
                 mask_safe=dataset_.mask_safe.copy(),
                 gti=dataset_.gti.copy(),
-                name='dataset N',
-                N_parameters=N_parameters,
-                penalty_sigma= sigma)
-        models = self.set_model()
-        bkg_model = FoVBackgroundModel(dataset_name=dataset_N.name)
+                name='dataset N')
+        models = self.set_model_N()
+        #bkg_spectralmodel = PowerLawNormNuisanceSpectralModel(
+        #            tilt=1,
+        #            norm=1,
+        #            norm_nuisance=0
+        #)
+        bkg_model = FoVBackgroundModel(dataset_name=dataset_N.name,
+                                      )#spectral_model = bkg_spectralmodel)
         bkg_model.parameters['tilt'].frozen  = False
         models.append(bkg_model)
         dataset_N.models = models
